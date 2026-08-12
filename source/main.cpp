4831649d4966a09595cd5c273f79c6793a19e790
Lakshay7203 <lakshay2003lahar07@gmail.com>
2026-08-11 21:22:12 -0400

    Initialize SDL3

--- source/main.cpp
+++ source/main.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "TrailTorque starting...\n";
+    if (!SDL_Init(SDL_INIT_VIDEO))
+    {
+        SDL_Log("SDL initialization failed: %s", SDL_GetError());
+        return 1;
+    }
+
+    SDL_Log("SDL initialized successfully!");
+
+    SDL_Quit();
 
     return 0;
 }
